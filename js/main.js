a10e2c504e8c922f8b472a55ad80ae9758b40809
Kolpsi <tykataf@gmail.com>
2019-06-11 19:11:56 +0500

    создал функцю отрисовки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
 'use strict';
 
 var map = document.querySelector('.map');
+var pinTemplate = document.querySelector('#pin')
+.content
+.querySelector('.map__pin');
 
 map.classList.remove('map--faded');
+
+var avatars = ['01.png', '02.png', '03.png', '04.png', '05.png', '06.png', '07.png', '08.png',];
+var types = ['palace', 'flat', 'house', 'bungalo'];
+
+var shuffleArray = function (array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array[i];
+}
+
+var createNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+var renderPin = function () {
+  var pin = pinTemplate.cloneNode(true);
+
+  pin.style.left = createNumber(1, 1200) + 'px';
+  pin.style.top = createNumber(130, 630) + 'px';
+  pin.querySelector('.map__img').src = 'img/avatars/user' + shuffleArray(avatars);
+  pin.querySelector('.map__img').alt = 'Заголовок';
+
+   return pin;
+};
+
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < 8; i++) {
+  fragment.appendChild(renderPin());
+};
+
+pinTemplate.appendChild(fragment);
+
+console.log(fragment.appendChild(renderPin()));
