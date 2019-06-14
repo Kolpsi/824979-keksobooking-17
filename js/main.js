6ba2ba48d06266caf0671e3670b3d73ab59c444f
Kolpsi <tykataf@gmail.com>
2019-06-14 18:57:25 +0500

    Работа над ошибками

--- js/main.js
+++ js/main.js
@@ -3,26 +3,57 @@
 var map = document.querySelector('.map');
 var PIN_HEIGHT = 70;
 var PIN_WIDTH = 50;
+var PIN_X = PIN_WIDTH / 2;
 var pinTemplate = document.querySelector('#pin')
 .content
 .querySelector('.map__pin');
+var avatars = [];
 
 map.classList.remove('map--faded');
 
-var avatars = ['01', '02', '03', '04', '05', '06', '07', '08'];
-// var types = ['palace', 'flat', 'house', 'bungalo'];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+// функция выбора рандомного элемента
+var getRandomElement = function (array) {
+  var index = Math.floor(Math.random() * array.length);
+  return array[index];
+};
+
 // функция поиска рандомного числа в промежутке min, max
 var createNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
+// функция создания номера аватара
+var createrNumberAvatar = function (numberAvatars) {
+  for (var i = 1; i < numberAvatars; i++) {
+  avatars.push('img/avatars/user0' + i + '.png');
+  };
+};
+
+createrNumberAvatar(9);
+// функция созданя сгенерированных JS объектов
+var createPinIformation = function () {
+  return {
+    author: {
+      avatar: createrNumberAvatar()
+    },
+    location: {
+      x:createNumber(1, 1200) - PIN_X + 'px',
+      y:createNumber(130, 630) - PIN_HEIGHT + 'px'
+    },
+    offer: {
+      type: getRandomElement(TYPES)
+    }
+  }
+};
+
 // функция отрисовки маркеров
 var renderPin = function () {
   var pin = pinTemplate.cloneNode(true);
 
-  pin.style.left = createNumber(1, 1200) - PIN_WIDTH / 2 + 'px';
-  pin.style.top = createNumber(130, 630) - PIN_HEIGHT + 'px';
-  pin.querySelector('.map__img').src = 'img/avatars/user' + avatars[i] + '.png';
-  pin.querySelector('.map__img').alt = 'заголовок объявления';
+  pin.style.left = createPinIformation().location.x;
+  pin.style.top = createPinIformation().location.y;
+  pin.querySelector('img').src = avatars[i];
+  pin.querySelector('img').alt = 'заголовок объявления';
 
   return pin;
 };
@@ -30,6 +61,7 @@ var renderPin = function () {
 var fragment = document.createDocumentFragment();
 
 for (var i = 0; i < 8; i++) {
+  createPinIformation()
   fragment.appendChild(renderPin());
 }
 
