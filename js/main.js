2ae03cab59fb30c216d2710ec474f4abba66c28b
Kolpsi <tykataf@gmail.com>
2019-06-15 00:44:49 +0500

    Исправления

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 'use strict';
 
-var map = document.querySelector('.map');
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var PIN_HEIGHT = 70;
 var PIN_WIDTH = 50;
 var PIN_X = PIN_WIDTH / 2;
+var map = document.querySelector('.map');
 var pinTemplate = document.querySelector('#pin')
-.content
-.querySelector('.map__pin');
-var avatars = [];
+  .content
+  .querySelector('.map__pin');
+var avatars = []
 
 map.classList.remove('map--faded');
 
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 // функция выбора рандомного элемента
 var getRandomElement = function (array) {
   var index = Math.floor(Math.random() * array.length);
   return array[index];
 };
+// функция создания номера аватара
+var createrNumberAvatar = function (numberAvatars) {
+  for (var i = 0; i < numberAvatars; i++) {
+  avatars.push(i);
+  };
+};
+
+createrNumberAvatar(9);
 
 // функция поиска рандомного числа в промежутке min, max
 var getNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
-// функция создания номера аватара
-var createrNumberAvatar = function (numberAvatars) {
-  for (var i = 1; i < numberAvatars; i++) {
-    avatars.push('img/avatars/user0' + i + '.png');
-  }
-};
-
-createrNumberAvatar(9);
 // функция созданя сгенерированных JS объектов
-var createPinIformation = function () {
+var createPinIformation = function (index) {
   return {
     author: {
-      avatar: createrNumberAvatar()
+      avatar: 'img/avatars/user0' + (index + 1) + '.png'
     },
     location: {
       x: getNumber(1, 1200) - PIN_X + 'px',
@@ -52,7 +52,7 @@ var renderPin = function () {
 
   pin.style.left = createPinIformation().location.x;
   pin.style.top = createPinIformation().location.y;
-  pin.querySelector('img').src = avatars[i];
+  pin.querySelector('img').src = createPinIformation(avatars[i]).author.avatar;
   pin.querySelector('img').alt = 'заголовок объявления';
 
   return pin;
@@ -61,8 +61,8 @@ var renderPin = function () {
 var fragment = document.createDocumentFragment();
 
 for (var i = 0; i < 8; i++) {
-  createPinIformation();
   fragment.appendChild(renderPin());
 }
 
 map.appendChild(fragment);
+
