4c82aa3be877c07b267375943de2bc01761f4676
Kolpsi <tykataf@gmail.com>
2019-06-24 23:56:27 +0500

    Реализация перетаскивания маркера по карте

--- js/main.js
+++ js/main.js
@@ -68,14 +68,9 @@ var onPinClick = function (evt) {
   evt.preventDefault();
   onMainPinActivated();
   map.appendChild(fragment);
-  mainPin.removeEventListener('click', onPinClick);
+  mainPin.removeEventListener('mousedown', onPinClick);
 };
 
-mainPin.addEventListener('click', onPinClick);
-
-mainPin.addEventListener('mouseup', function () {
-  setAddress(mainPin);
-});
 // функция активирования страницы
 var onMainPinActivated = function () {
   map.classList.remove('map--faded');
@@ -136,3 +131,62 @@ var onChangeType = function () {
 type.addEventListener('change', onChangeType);
 timeIn.addEventListener('change', onChangeTime);
 timeOut.addEventListener('change', onChangeTime);
+
+// Задание №5
+var mainMap = document.querySelector('.map__pins');
+
+mainPin.addEventListener('mousedown', onPinClick);
+
+mainPin.addEventListener('mousedown', function (evt) {
+  setAddress(mainPin);
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var TOP_LIMITER_PIN = 130;
+    var BOTTOM_LIMITER_PIN = 630;
+    var fieldCoords = mainMap.getBoundingClientRect();
+    var fieldInnerCoords = {
+        top: fieldCoords.top + mainMap.clientTop,
+        left: fieldCoords.left + mainMap.clientLeft
+      };
+
+      var pinCoords = {
+        top: event.clientY - fieldInnerCoords.top - mainPin.clientHeight,
+        left: event.clientX - fieldInnerCoords.left - mainPin.clientWidth
+      };
+
+      // вылезает за верхнюю границу - разместить по ней
+      if (pinCoords.top < TOP_LIMITER_PIN) pinCoords.top = TOP_LIMITER_PIN;
+
+      // вылезает за левую границу - разместить по ней
+      if (pinCoords.left < mainMap.clientLeft) pinCoords.left = mainMap.clientLeft;
+
+
+      // вылезает за правую границу - разместить по ней
+      if (pinCoords.left + mainPin.clientWidth > mainMap.clientWidth) {
+        pinCoords.left = mainMap.clientWidth - mainPin.clientWidth;
+      }
+
+      // вылезает за нижнюю границу - разместить по ней
+      if (pinCoords.top + mainPin.clientHeight > mainMap.clientHeight) {
+        pinCoords.top = BOTTOM_LIMITER_PIN;
+      }
+
+       mainPin.style.left = pinCoords.left + 'px';
+       mainPin.style.top = pinCoords.top + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    setAddress(mainPin);
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+
+console.log(mainMap.clientHeight);
